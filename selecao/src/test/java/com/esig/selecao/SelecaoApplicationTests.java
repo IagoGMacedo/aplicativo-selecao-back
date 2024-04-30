040bc214559b8d57b847f31556b455f160fc97de
IagoGMacedo <iagognobre@gmail.com>
2024-04-30 18:31:45 -0300

    commit inicial

---
+++ selecao/src/test/java/com/esig/selecao/SelecaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esig.selecao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelecaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
